b605b592a0e8cfaea8779a9a85ce9e0c03558b8f
tantinoroj <tan.t@chevron.com>
2024-12-20 13:14:10 +0700

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,9 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+
+
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
