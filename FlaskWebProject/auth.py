7076865076429a2611ec6fe3d777d58cb71016bf
tantinoroj <tan.t@chevron.com>
2025-01-11 05:18:08 +0700

    Update auth.py

--- FlaskWebProject/auth.py
+++ FlaskWebProject/auth.py
@@ -3,15 +3,15 @@ from flask import render_template
 import msal
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Change this to a random secret key
+app.secret_key = '3kM8Q~dfKLfiSpRHDvQpJfEAJSGhAbLgr6i_5dsR'  # Change this to a random secret key
 app.config['SESSION_TYPE'] = 'filesystem'
 
 # Azure AD configuration
-CLIENT_ID = 'your_client_id'  # Application (client) ID
-CLIENT_SECRET = 'your_client_secret'  # Client secret
-TENANT_ID = 'your_tenant_id'  # Directory (tenant) ID
-AUTHORITY = f'https://login.microsoftonline.com/{TENANT_ID}'
-REDIRECT_URI = 'http://localhost:5000/getAToken'  # Redirect URI
+CLIENT_ID = '614fd590-1b5f-4e1f-bfc1-812ed33f9895'  # Application (client) ID
+CLIENT_SECRET = 'kRY8Q~UQLwLj4yBomCVIeZgMCgI0peS7K9LA4bx2'  # Client secret
+TENANT_ID = 'ff873fe8-6631-416d-9262-bdbd56117dae'  # Directory (tenant) ID
+AUTHORITY = f'https://login.microsoftonline.com/ff873fe8-6631-416d-9262-bdbd56117dae'
+REDIRECT_URI = 'https://udacitycms-hee2d4eyhgabgqa2.southeastasia-01.azurewebsites.net'  # Redirect URI
 
 # Scopes for the API
 SCOPE = ['User.Read']  # Add other scopes as needed
