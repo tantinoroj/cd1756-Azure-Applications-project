fa08459ee1736967e7a3bc697fcb4a6ef1880240
tantinoroj <tan.t@chevron.com>
2025-01-11 09:42:36 +0700

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -64,22 +64,22 @@ def login():
         Config.SCOPE,
         redirect_uri=url_for('authorized', _external=True))
     return redirect(session["flow"]["auth_uri"])
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('home'))
-    # form = LoginForm()
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(username=form.username.data).first()
-    #     if user is None or not user.check_password(form.password.data):
-    #         flash('Invalid username or password')
-    #         return redirect(url_for('login'))
-    #     login_user(user, remember=form.remember_me.data)
-    #     next_page = request.args.get('next')
-    #     if not next_page or url_parse(next_page).netloc != '':
-    #         next_page = url_for('home')
-    #     return redirect(next_page)
-    # session["state"] = str(uuid.uuid4())
-    # auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
-    # return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
+    session["state"] = str(uuid.uuid4())
+    auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
+    return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
 
 @app.route(Config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
 def authorized():
