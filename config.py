23e7bc5e53e24b1b52501a70a335c221faa1bb0f
tantinoroj <tan.t@chevron.com>
2025-03-16 15:01:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,23 +3,23 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = 'ac2df92a-66cf-4f47-875b-f5d027c33934'
+    SECRET_KEY = 'dac1d194-bc21-41a4-ae76-9faf0f2d63cb'
 
-    BLOB_ACCOUNT = 'images99'
-    BLOB_STORAGE_KEY = 'IXy2NpG/ghgh6dRlhR5JBBi8IlK8MDxIJkBGdQBc8XlZs5Jp3DGD6PVQQS1/0JHXdtWT/KQuf+aO+AStesXKRA=='
+    BLOB_ACCOUNT = 'cmstan'
+    BLOB_STORAGE_KEY = 'l3dXxrnSbhUWkB4qBZy5bPjzRGeqnK0KXuJbMVTd1OP5+VfMnAZg51MKJsYDpveeMIzb4lVY/+x/+AStUpeGKA=='
     BLOB_CONTAINER = 'images'
 
-    SQL_SERVER = 'cms-server'
+    SQL_SERVER = 'cmstan'
     SQL_DATABASE = 'cms'
-    SQL_USER_NAME = 'cmsadmin'
-    SQL_PASSWORD = 'CMS4dmin'
+    SQL_USER_NAME = 'sqladmin'
+    SQL_PASSWORD = '!pwd1234'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "kRY8Q~UQLwLj4yBomCVIeZgMCgI0peS7K9LA4bx2"
+    CLIENT_SECRET = "J9s8Q~Nf5f2GBa6X46mByIZv1qig1MMaYPCA3awK"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -28,11 +28,11 @@ class Config(object):
         raise ValueError("Need to define CLIENT_SECRET environment variable")
 
     # AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
-    AUTHORITY = "https://login.microsoftonline.com/ff873fe8-6631-416d-9262-bdbd56117dae"
+    AUTHORITY = "https://login.microsoftonline.com/f958e84a-92b8-439f-a62d-4f45996b6d07"
 
-    CLIENT_ID = "614fd590-1b5f-4e1f-bfc1-812ed33f9895"
+    CLIENT_ID = "e2b17eb9-7334-4c3a-ba90-bbd09b4aa1d4"
 
-    REDIRECT_PATH = "https://udacitycms-hee2d4eyhgabgqa2.southeastasia-01.azurewebsites.net/"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    REDIRECT_PATH = "https://cmstan-cbb6f4a4auavfpab.southeastasia-01.azurewebsites.net/"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
